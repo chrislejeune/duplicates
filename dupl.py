d13c2e56c851c45750ec88fcc0dc7ade24f7e4d8
chrislejeune <gnurulz@gmail.com>
2020-01-27 21:54:49 +0000

    added function to find duplicate hashes

--- dupl.py
+++ dupl.py
@@ -2,6 +2,19 @@ import os
 import sys
 import hashlib
 
+def findDup(parentFolder):
+    dups = {}
+    for dirName, subdirs, filelist in os.walk(parentFolder):
+        print('Scanning %s...' % dirName)
+        for filename in fileList:
+            path = os.path.join(dirName, filename)
+            file_hash = hashfile(path)
+            if file_hash in dups:
+                dups[file_hash].append(path)
+            else:
+                dups[file_hash] = [path]
+    return dups
+
 # function to get the MD5 hash of a file 
 
 def hashfile(path, blocksize = 65536):
@@ -14,6 +27,4 @@ def hashfile(path, blocksize = 65536):
     afile.close()
     return hasher.hexdigest()
     
-# function to scan directories for duplicated files
-
 
