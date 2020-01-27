41645fe062818564a69803e8de19999af75699fd
chrislejeune <gnurulz@gmail.com>
2020-01-27 21:31:53 +0000

    initial commit

---
+++ dupl.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import hashlib
+
+# function to get the MD5 hash of a file 
+
+def hashfile(path, blocksize = 65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+    
+# function to scan directories for duplicated files
+
+
